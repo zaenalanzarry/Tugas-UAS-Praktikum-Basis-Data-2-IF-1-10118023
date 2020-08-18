a129778defe281310dab9e88a3537593817d36a0
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2020-08-18 20:27:15 +0700

    Add files via upload

---
+++ mahasiswa/component/menu.php
@@ -0,0 +1,25 @@
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li>
+                            <a href="index.php" class="waves-effect"><i class="fa fa-clock-o m-r-10" aria-hidden="true"></i>Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="profile.php" class="waves-effect"><i class="fa fa-user m-r-10" aria-hidden="true"></i>Profile</a>
+                        </li>
+                        <li>
+                            <a href="riwayat_nilai.php" class="waves-effect"><i class="fa fa-table m-r-10" aria-hidden="true"></i>Riwayat Nilai</a>
+                        </li>
+                        <li>
+                            <a href="krs.php" class="waves-effect"><i class="fa fa-font m-r-10" aria-hidden="true"></i>Pengisian Rencana Studi</a>
+                        </li>
+                    </ul>
+ 
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
