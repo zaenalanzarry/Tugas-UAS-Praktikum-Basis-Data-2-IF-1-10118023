431106b63bc98abc36e0b57bffbe182474b32d4c
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2020-08-18 20:18:40 +0700

    Add files via upload

---
+++ dosen/component/menu.php
@@ -0,0 +1,22 @@
+        <aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li>
+                            <a href="index.php" class="waves-effect"><i class="fa fa-clock-o m-r-10" aria-hidden="true"></i>Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="profile.php" class="waves-effect"><i class="fa fa-user m-r-10" aria-hidden="true"></i>Profile</a>
+                        </li>
+                        <li>
+                            <a href="nilai.php" class="waves-effect"><i class="fa fa-table m-r-10" aria-hidden="true"></i>Pengisian Nilai</a>
+                        </li>
+                    </ul>
+ 
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
